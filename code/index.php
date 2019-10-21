0a021b05c4c434a7a72fb8208f4575ee14a49e25
YasienDwieb <yasien.dwieb@gmail.com>
2019-10-21 17:46:28 +0200

    cleanup

--- code/index.php
+++ code/index.php
@@ -2,16 +2,13 @@
 $a = 9;
 $b = 19;
 echo "hello world: {$a}+{$b} = " . strval(intval($a) + intval($b))."<br/>";
-// $host="db";
+
 $host="db";
 $db=getenv('MYSQL_DATABASE');
-// $user=getenv('MYSQL_USER');
-// $pass=getenv('MYSQL_PASSWORD');
-
-$user='root';
-$pass='rootPass';
-
+$user=getenv('MYSQL_USER');
+$pass=getenv('MYSQL_PASSWORD');
 $port=getenv('MYSQL_PORT');
+
 $conn= mysqli_connect($host,$user,$pass,$db,$port);
 
 var_dump($conn);
