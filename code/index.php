a49ed360ee25fc5c79d78d96edf7780c664930b8
YasienDwieb <yasien.dwieb@gmail.com>
2019-10-24 02:30:39 +0200

    replace web servers with Swoole server in the same container with php-fpm

--- code/index.php
+++ code/index.php
@@ -1,8 +1,13 @@
-<?php 
-   //Connecting to Redis server on localhost 
-   $redis = new Redis(); 
-   $redis->connect('redis', 6379); 
-   echo "Connection to server sucessfully"; 
-   //check whether server is running or not 
-   echo "Server is running: ".$redis->ping(); 
-?>
+<?php
+$http = new swoole_http_server("0.0.0.0", 8080);
+
+$http->on("start", function ($server) {
+    echo "Swoole http server is started at http://127.0.0.1:8080\n";
+});
+
+$http->on("request", function ($request, $response) {
+    $response->header("Content-Type", "text/plain");
+    $response->end(print_r("Hello world", true));
+});
+
+$http->start();
